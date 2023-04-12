dd2adc84184aa4835bc791ae74121dc3cfe7bb3b
msoliviawatt <msoliviawatt@gmail.com>
2023-04-12 10:16:08 -0400

    blaeck ekys

--- BlackKey.java
+++ BlackKey.java
@@ -1,6 +1,22 @@
 import java.awt.*;
 
 public class BlackKey {
-    public BlackKey() {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private boolean isPressed;
+    private Image blackKey;
+    private Image blackKeyDown;
+
+    //constructor
+    public BlackKey(int x, int y, int width, int height, Image blackKey, Image blackKeyDown) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.blackKey = blackKey;
+        this.blackKeyDown = blackKeyDown;
+        this.isPressed = false;
     }
 }
--- Piano.java
+++ Piano.java
@@ -1,4 +1,31 @@
+import java.awt.*;
+
+import javax.swing.ImageIcon;
+
 public class Piano {
     private final int WHITE_KEYS = 52;
     private int BLACK_Keys = 36;
+
+    private BlackKey[] blackKeys = new BlackKey[BLACK_Keys];
+    private WhiteKey[] whiteKeys = new WhiteKey[WHITE_KEYS];
+
+    //black key images
+    private Image blackKey = new ImageIcon("blackKey.png").getImage();
+    private Image blackKeyDown = new ImageIcon("blackKeyDown.png").getImage();
+
+    public Piano() {
+        //white keys
+        // for (int i = 0; i < WHITE_KEYS; i++) {
+        //     whiteKeys[i] = new WhiteKey(i * 20, 0, 20, 100);
+        // }
+
+        //black keys
+        int blackKeyIndex = 0;
+        for (int i = 0; i < WHITE_KEYS; i++) {
+            if (i % 7 == 2 || i % 7 == 6) {
+                blackKeys[blackKeyIndex] = new BlackKey(i * 20, 0, 20, 100, blackKey, blackKeyDown);
+                blackKeyIndex++;
+            }
+        }
+    }
 }
