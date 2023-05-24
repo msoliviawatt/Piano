18864cfb091716719b790f8471bf75486c42ffa0
aakashn123 <129410008+aakashn123@users.noreply.github.com>
2023-05-23 22:27:01 -0400

    comments

--- Key.java
+++ Key.java
@@ -4,67 +4,71 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Key extends Audio {
-    private static int totalKeys = 48;
-    private String keyName;
-    private File audioFile;
-    private JButton keyButton;
-    private boolean keyPressed;
+   private static int totalKeys = 48;
+   private String keyName;
+   private File audioFile;
+   private JButton keyButton;
+   private boolean keyPressed;
 
 
-    public Key(String s, JButton b) {
-        keyName = s;
-        keyButton = b;
-        audioFile = new File("AudioFiles/" + keyName + ".mp3");
-
-        keyButton.addMouseListener(new java.awt.event.MouseAdapter() {
+   public Key(String s, JButton b) {
+      keyName = s;
+      keyButton = b;
+      audioFile = new File("AudioFiles/" + keyName + ".mp3");
+   
+       //plays sound file when note is clicked
+      keyButton.addMouseListener(
+         new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                playKey();
-                System.out.println(keyName);
-                PianoLayout.frame.requestFocusInWindow();
+               playKey();
+               PianoLayout.frame.requestFocusInWindow();
             }
-
+         
             public void mousePressed(java.awt.event.MouseEvent evt) {
-                setKeyPressed(true);
+               setKeyPressed(true);
             }
-
+         
             public void mouseReleased(java.awt.event.MouseEvent evt) {
-                setKeyPressed(false);
+               setKeyPressed(false);
             }
-        });
-    }
+         });
+   }
 
-    public String getKey() {
-        return keyName;
-    }
+   public String getKey() {
+      return keyName;
+   }
 
-    public File getFile() {
-        return audioFile;
-    }
+   public File getFile() {
+      return audioFile;
+   }
 
-    public JButton getButton() {
-        return keyButton;
-    }
+   public JButton getButton() {
+      return keyButton;
+   }
 
-    public void setKey(String s) {
-        keyName = s;
-    }
+   public void setKey(String s) {
+      keyName = s;
+   }
 
-    public void setFile(File f) {
-        audioFile = f;
-    }
+   public void setFile(File f) {
+      audioFile = f;
+   }
 
-    public void setButton(JButton b) {
-        keyButton = b;
-    }
+   public void setButton(JButton b) {
+      keyButton = b;
+   }
 
-    public void setKeyPressed(boolean pressed) {
-        keyPressed = pressed;
-    }
+   public void setKeyPressed(boolean pressed) {
+      keyPressed = pressed;
+   }
 
-    public static Key[] keys = new Key[totalKeys];
+   public static Key[] keys = new Key[totalKeys];
 
-    public void playKey() {
-        String keyName = getKey();
-        playSound("AudioFiles/" + keyName + ".mp3");
-    } 
+   //plays audio
+   public void playKey() {
+      String keyName = getKey();
+      playSound("AudioFiles/" + keyName + ".mp3");
+      System.out.println(keyName);
+   
+   } 
 }
